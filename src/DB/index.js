34814f2e1f36c243b0d553ff0756c6665892e55d
Rishi-Mittal-daffodil <rishi.mittal@daffodilsw.com>
2024-09-13 15:29:33 +0530

    utils files added

---
+++ src/DB/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const DB_CONNECT = async () => {
+  try {
+    const instanceConnection = await mongoose.connect(
+      `${process.env.DB_URI}/${DB_NAME}`
+    );
+    console.log(
+      "DB Connected SucessFully ",
+      instanceConnection.connection.host
+    );
+  } catch (err) {
+    console.log("Mongo connection failed", err);
+    process.exit(1);
+  }
+};
+export default DB_CONNECT;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express, { urlencoded } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import DB_CONNECT from "./DB/index.js";
+import cookieParser from "cookie-parser";
+const app = express();
+dotenv.config();
+
+
+app.use(express.json({limit: "16kb"})); 
+app.use(urlencoded({extended:true , limit:"16kb"}));
+app.use(express.static("public")) ; 
+app.use(cookieParser()) ; 
+
+
+
+
+
+
+
+const PORT = 8000;
+DB_CONNECT()
+  .then(() => {
+    app.listen(PORT || 8000, () => {
+      console.log(`Server Connected at ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error occured in DB Connection", err);
+  });
