0f5e87de8ea50206f28b231cf2f3389ac666523a
Rishi-Mittal-daffodil <rishi.mittal@daffodilsw.com>
2024-09-16 16:57:42 +0530

    create user route

--- src/index.js
+++ src/index.js
@@ -3,18 +3,32 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import DB_CONNECT from "./DB/index.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app = express();
 dotenv.config();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-const PORT = 8000;
+//importing routes
+import userRoutes from "./routes/user.router.js";
+
+
+//use routes in middleware  .  
+app.use("/api/v1/user" , userRoutes) ; 
+
+const PORT = process.env.PORT || 8000;
 DB_CONNECT()
   .then(() => {
-    app.listen(PORT || 8000, () => {
+    app.listen(PORT, () => {
       console.log(`Server Connected at ${PORT}`);
     });
   })
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,43 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from fs ; 
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_API_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
+const uploadOnCloudinary =  async (localFilePath) =>{
+  try{
+    if(!localFilePath)  return null  ; 
+    //upload file on cloudinary . 
+    const response = await cloudinary.uploader.upload(
+      localFilePath,
+      {
+        resource_type : "auto" , 
+      }
+    )
+    console.log("file has been upload sucessfully " , response.url);
+    return response  ;
+
+  }
+  catch(error){
+    // if file uploading failed so we have to localfilepath . 
+    fs.unlinkSync(localFilePath) ; 
+    return null ;
+  }
+}
+
+
+const uploadResult = await cloudinary.uploader.upload(
+    "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+    {
+      public_id: "shoes",
+    }
+  )
+  .catch((error) => {
+    console.log(error);
+  });
+
+console.log(uploadResult);
